ab57d6a5c5f2bcfbcced74841f520fc4ca6c1660
liumimi <liumimi346@gmail.com>
2026-05-11 19:41:24 +0800

    fix supabase env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rjhpiaxipntqjkfqfbze.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqaHBpYXhpcG50cWprZnFmYnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NzM0MjUsImV4cCI6MjA5MjE0OTQyNX0.RuTstnmLjCps04Pi_MZPp7fl0x0gH9zEHvsxlW933Tc";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
